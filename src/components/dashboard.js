9ce1499966474ba4fab8fd9a13dc84fec4998796
Hashini <prabuddhikarph@gmail.com>
2024-07-18 20:48:06 +0530

    updated1

---
+++ src/components/dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'boxicons/css/boxicons.min.css';
+import '../style.css';
+import CalendarHeatmapComponent from '../layouts/CalendarHeatmap';
+import Sidebar from '../layouts/Sidebar';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Sidebar></Sidebar>
+        </div>
+    );
+}
+
+export default Dashboard;
