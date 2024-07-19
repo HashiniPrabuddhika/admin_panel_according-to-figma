ca0c3553a369f8532901db8c8306d4ae15a164a3
Hashini <prabuddhikarph@gmail.com>
2024-07-19 11:32:26 +0530

    updated5

--- src/layouts/meal.js
+++ src/layouts/meal.js
@@ -34,12 +34,28 @@ const meal = () => {
                                 <td className="word">Goal</td>
                             </tr>
                             <tr>
-                            <td colSpan="3">
-                  <div className="rank-container">
-                    <div className="line"></div>
-                    <div className="rank" style={{ left: '25%' }}>25%</div>
-                    <div className="rank" style={{ left: '50%' }}>50%</div>
-                    <div className="rank" style={{ left: '75%' }}>75%</div>
+                            <td>
+                  <div className="progress-line-container">
+                    <div className="progress-line">
+                      <div className="progress-filled" style={{ width: '70%', backgroundColor: '#FF738F' }}></div>
+                    </div>
+    
+                  </div>
+                </td>
+                <td>
+                  <div className="progress-line-container">
+                    <div className="progress-line">
+                      <div className="progress-filled" style={{ width: '50%' , backgroundColor: '#738FFF'}}></div>
+                    </div>
+                   
+                  </div>
+                </td>
+                <td>
+                  <div className="progress-line-container">
+                    <div className="progress-line">
+                      <div className="progress-filled" style={{ width: '30%', backgroundColor: '#B8EEA4' }}></div>
+                    </div>
+                    
                   </div>
                 </td>
                                  </tr>
@@ -47,124 +63,7 @@ const meal = () => {
                     </div>
             </div>
 
-            <section id="content">
-                <div className="main-container">
-                    <div className="notification">Advice! Connect your Apple Watch for better results.
-                        <div className="circle-notification-container">
-                            <div className="circle-notification">🤖</div>
-                        </div>
-                    </div>
-                    <div className="Popular_Training"><p>Popular Training</p></div>
-                    <div className="training-container">
-                        <div className="training-item">
-                            <div className="circle-text-container">
-                                <div className="circle-notification">🦍</div>
-                            </div>
-                            <span className="texts">Power Training</span>
-                            <span className="texts2">395 kcal / h</span>
-                        </div>
-                        <div className="training-item">
-                            <div className="circle-text-container">
-                                <div className="circle-notification">🐍</div>
-                            </div>
-                            <span className="texts">Yoga Training</span>
-                            <span className="texts2">395 kcal / h</span>
-                        </div>
-                        <div className="training-item">
-                            <div className="circle-text-container">
-                                <div className="circle-notification">🐌</div>
-                            </div>
-                            <span className="texts">Stretching</span>
-                            <span className="texts2">395 kcal / h</span>
-                        </div>
-                    </div>
-                    <div className="training-container">
-                        <div className="Activity_Statistic">
-                            <p className="activity">Activity Statistic</p>
-                            <CalendarHeatmapComponent />
-                        </div>
-                        <div className="myTraining">
-                            <div className="myTraining-header">
-                                <p className="activity">My Training</p>
-                                <div className="circle-training-container">
-                                    <i className="bx bx-plus-medical"></i>
-                                </div>
-                            </div>
-                            <div className="table-container">
-                                <div className="table-row">
-                                    <div className="table-cell light-gray">Training</div>
-                                    <div className="table-cell black-text">TRX Cardio</div>
-                                </div>
-                                <div className="table-row">
-                                    <div className="table-cell light-gray">Burned</div>
-                                    <div className="table-cell black-text">350 kcal</div>
-                                </div>
-                                <div className="table-row">
-                                    <div className="table-cell light-gray">Spend</div>
-                                    <div className="table-cell black-text">1h 45m</div>
-                                </div>
-
-                                <div className='table2'>
-                                    <div className="table-row">
-                                        <div className="table-cell light-gray">Training</div>
-                                        <div className="table-cell black-text">Stretching</div>
-                                    </div>
-                                    <div className="table-row">
-                                        <div className="table-cell light-gray">Burned</div>
-                                        <div className="table-cell black-text">180 kcal</div>
-                                    </div>
-                                    <div className="table-row">
-                                        <div className="table-cell light-gray">Spend</div>
-                                        <div className="table-cell black-text">30 m</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div className='heartRateRow'>
-                        <div className='heartRate'>
-                            <p className="activity">Heart Rate</p>
-                            <div className='heartRateContainer'>
-                                <div className='heartRateInfo'>
-                                    <div className="rate-details">
-                                        <span className='textRate'>💓 75 bpm</span>
-                                        <span className='textRate2'>Health zone</span>
-                                    </div>
-                                    <div className="circle-heartRate-container">
-                                        <i className="bx bxs-doughnut-chart"></i>
-                                    </div>
-                                    <p className="heart-text">Additional Info</p>
-                                </div>
-
-
-                            </div>
-                        </div>
-
-                        <div className='heartRate'>
-                            <p className="activity">Water Balance</p>
-                            <div className='heartRateContainer'>
-                                <div className='heartRateInfo'>
-                                    <div className="rate-details">
-                                        <span className='textRate2'>🐳 <span className="light-gray">Drunk</span></span>
-                                        <span className='textRate'>1250 ml<span className="light-gray">/2000 ml</span></span>
-                                    </div>
-                                    <div className="circle-heartRate-container">
-                                        <i className="bx bx-plus-medical"></i>
-                                    </div>
-                                    <p className="heart-text">Add Water</p>
-                                </div>
-
-
-                            </div>
-                        </div>
-
-                    </div>
-
-
-                </div>
-            </section>
+          
     </div>
   )
 }
