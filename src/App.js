f5b5094e27d9a12deee9b25041352b808f839a2d
Hashini <prabuddhikarph@gmail.com>
2024-07-19 10:53:02 +0530

    updated3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/dashboard'; 
 import Doctor from './components/doctor';
